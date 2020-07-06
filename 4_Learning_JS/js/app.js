7ec559efba7de58ec141fd7bffa2ea3e511fdb13
Alejo Rondon <blackphotografy@gmail.com>
2020-07-06 05:54:55 -0500

    [LOAD]Charging base project

---
+++ 4_Learning_JS/js/app.js
@@ -0,0 +1,23 @@
+//? 'Global and local variables'
+console.log("'Global and local variables'")
+
+// global variable
+var message = 'Hello'
+function say() {
+  // local variable
+  var message = 'Hi'
+  console.log('say -> message', message)
+}
+say() // Hi
+console.log('message', message)
+
+//? 'Accessing global variable inside the function'
+console.log("'Accessing global variable inside the function'")
+
+function sayTwo() {
+  // local variable
+  message = 'Hi'
+  console.log(`sayTwo -> message`, message)
+}
+sayTwo() // Hi
+console.log('message', message) //Hi
---
+++ 4_Learning_JS/notes.md
@@ -0,0 +1,5 @@
+Use <script> element to include a JavaScript file in a HTML page.
+
+The async attribute of the <script> element instructs the web browser to fetch the JavaScript file in parallel and then parse and execute as soon as the JavaScript file is available.
+
+The defer attribute of the <script> element allows the web browser to execute the JavaScript file after the document has been parsed.
